d5bd382c9098e182431bd32967f452656873b58f
MOMOTRONICS <151157502+MOMOTRONICS@users.noreply.github.com>
2023-12-02 22:07:51 +0700

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/home_page.dart';
+import 'package:royal_login/pages/login_page.dart';
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: HomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
